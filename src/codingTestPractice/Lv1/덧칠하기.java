03379bed88a68ddcd8988d408cf03981a260c404
e-7281998 <jejjcs43@naver.com>
2023-03-27 22:34:17 +0900

    '덧칠하기[완료]'

--- src/codingTestPractice/Lv1/덧칠하기.java
+++ src/codingTestPractice/Lv1/덧칠하기.java
@@ -1,23 +1,18 @@
 package codingTestPractice.Lv1;
 
-//칠해야하는 구역의 처음과 끝의 차이 구하고
-// 롤러의 길이로 나누기
-//롤러의 길이와 차기가 
-
-//배열을 하나씩 돌면서 차이구하고, 그 차이를 롤러의 길이에서 하나씩 줄이기
-
 public class 덧칠하기 {
 	//벽 면적, 롤러 길이, 칠해야 하는 구역
 	public static int solution(int n, int m, int[] section) {
         int answer = 0;
-        int cnt = m;
-        int sub = 0;
-        
-        for(int i=1; i<section.length; i++) {
-        	sub = section[i] - section[i-1];	//1,2 값의 차이
-        	cnt += sub;
+        int paint = 0;
+
+        for(int num : section) {
+        	if(paint < num) {
+            	paint = num+m-1;
+            	answer++;
+        	}
         }
-        answer = cnt/m;
+        
         return answer;
     }
 	public static void main(String[] args) {
