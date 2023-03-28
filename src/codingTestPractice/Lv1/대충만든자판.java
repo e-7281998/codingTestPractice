3b8e8ea202cc4476327559a7289a02663d947ecf
e-7281998 <jejjcs43@naver.com>
2023-03-29 01:06:51 +0900

    '대충만든자판...'

--- src/codingTestPractice/Lv1/대충만든자판.java
+++ src/codingTestPractice/Lv1/대충만든자판.java
@@ -1,26 +1,54 @@
 package codingTestPractice.Lv1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public interface 대충만든자판 {
 	public static int[] solution(String[] keymap, String[] targets) {
-		int[] answer = {};
-		
-		for(int i=0; i<targets.length; i++) {
-			String[] target = targets[i].split("");
+		 int[] answer = new int[targets.length];
+			Map<String, Integer> map = new HashMap<>();
 			
-			for(String s: target) {
-				int min=1;
-				for(String key: keymap) {
-					
-					int find = key.indexOf(s) +1;
-					
-					min = Math.min(find, min);
-				}
-				answer[i] += min;
-			}
-			System.out.println(answer);
-		}
-		
-        return answer;
+			Arrays.stream(keymap).forEach(
+						key -> {
+							String[] keys = key.split("");
+							Arrays.stream(keys).forEach(
+										k -> {
+											int n = key.indexOf(k)+1;
+											if(map.get(k) == null || map.get(k) > n) {
+												map.put(k, n);										
+											}
+										}
+									);
+							}
+					);
+			Arrays.stream(targets).forEach(
+						target -> {
+							int i = 0;
+							String[] tg = target.split("");
+							Stack<Integer> st = new Stack<>();
+							Arrays.stream(tg).forEach(
+										t -> 
+											st.push(map.get(t))
+									);
+							for(String a : tg) {
+								int n = st.pop();
+								System.out.println(n);
+//								if(n == -1) {
+//									answer[i] = -1;
+//									break;
+//								}
+								
+								answer[i] += n;
+							}
+							i++;
+							System.out.println("--------------");
+						}
+					);
+			System.out.println(answer[0]);
+			System.out.println(answer[1]);
+	        return answer;
     }
 	public static void main(String[] args) {
 		String[] farr1 = {"ABACD", "BCEFD"};	//keymap
@@ -31,7 +59,6 @@ public interface 대충만든자판 {
 		
 		String[] tarr1 = {"AGZ", "BSSS"};
 		String[] tarr2 = {"ASA","BGZ"};
-		
-		System.out.println(solution(farr1, farr2));
+		solution(farr1, farr2);
 	}
 }
