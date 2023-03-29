75268b28495d836a167a14f422162e56a550c495
e-7281998 <jejjcs43@naver.com>
2023-03-29 15:06:00 +0900

    대충만든자판[완료]

--- src/codingTestPractice/Lv1/대충만든자판.java
+++ src/codingTestPractice/Lv1/대충만든자판.java
@@ -25,29 +25,34 @@ public interface 대충만든자판 {
 					);
 			Arrays.stream(targets).forEach(
 						target -> {
-							int i = 0;
+							int sum = -1;
 							String[] tg = target.split("");
 							Stack<Integer> st = new Stack<>();
 							Arrays.stream(tg).forEach(
-										t -> 
-											st.push(map.get(t))
+										t -> {
+											if(map.containsKey(t))
+												st.push(map.get(t));
+										}
 									);
-							for(String a : tg) {
-								int n = st.pop();
-								System.out.println(n);
-//								if(n == -1) {
-//									answer[i] = -1;
-//									break;
-//								}
-								
-								answer[i] += n;
+							if(st.size()!= 0) {
+								sum = 0;
+								for(String a : tg) {
+									if(st.size()==0) {
+										sum = -1;
+										break;
+									}
+									int n = st.pop();
+									sum += n;
+								}
 							}
-							i++;
-							System.out.println("--------------");
+							for(int i=0; i<targets.length; i++) {
+								if(answer[i]==0) {
+									answer[i] = sum;
+									break;
+								}
+							}
 						}
 					);
-			System.out.println(answer[0]);
-			System.out.println(answer[1]);
 	        return answer;
     }
 	public static void main(String[] args) {
@@ -59,6 +64,9 @@ public interface 대충만든자판 {
 		
 		String[] tarr1 = {"AGZ", "BSSS"};
 		String[] tarr2 = {"ASA","BGZ"};
+		
 		solution(farr1, farr2);
+		solution(sarr1, sarr2);
+		solution(tarr1, tarr2);
 	}
 }
