bcc98d1e21fee7084930df5da3b7c3c5109bc15b
e-7281998 <jejjcs43@naver.com>
2023-04-01 15:19:57 +0900

    카드뭉치[완료]

--- src/codingTestPractice/Lv1/카드뭉치.java
+++ src/codingTestPractice/Lv1/카드뭉치.java
@@ -2,10 +2,32 @@ package codingTestPractice.Lv1;
 
 public class 카드뭉치 {
 	public static String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        return answer;
-    }
+		String answer = "Yes";
+		int n1 = 0, n2 = 0;
+
+		for (String str : goal) {
+			if (n1 < cards1.length && cards1[n1].equals(str))
+				n1++;
+			else if (n2 < cards2.length && cards2[n2].equals(str))
+				n2++;
+			else {
+				answer = "No";
+				break;
+			}
+		}
+		return answer;
+	}
+
 	public static void main(String[] args) {
+		String cards1[] = {"i", "drink", "water"};
+		String cards2[] = {"want", "to"};
+		String goal[] = {"i", "want", "to", "drink", "water"};
+		System.out.println(solution(cards1, cards2, goal));// "Yes"
+		
+		String cards11[] = {"i", "water", "drink"};
+		String cards22[] = {"want", "to"};
+		String goal2[] = {"i", "want", "to", "drink", "water"};
+		System.out.println(solution(cards11, cards22, goal2));// "No"
 		
 	}
 }
