c42ac0cdc1b4b0d5845b9abdfd4ccc1f0a2c2598
e-7281998 <jejjcs43@naver.com>
2023-04-05 17:50:07 +0900

    팩토리얼0의개수...

---
+++ src/codingTestPractice/silver/팩토리얼0의개수.java
@@ -0,0 +1,13 @@
+package codingTestPractice.silver;
+
+import java.util.Scanner;
+
+public class 팩토리얼0의개수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// int num = sc.nextInt();
+		int num = 50;
+		int min = Math.min(num / 5, num / 2);
+		System.out.println(min);
+	}
+}
