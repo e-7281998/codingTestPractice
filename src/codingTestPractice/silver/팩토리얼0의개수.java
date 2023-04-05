04bb5796b31ed0dde0b35bad3d0ed3e51940c333
e-7281998 <jejjcs43@naver.com>
2023-04-05 22:49:52 +0900

    팩토리얼 0의 개수[완료]

--- src/codingTestPractice/silver/팩토리얼0의개수.java
+++ src/codingTestPractice/silver/팩토리얼0의개수.java
@@ -1,13 +1,20 @@
 package codingTestPractice.silver;
 
+import java.io.Console;
 import java.util.Scanner;
 
 public class 팩토리얼0의개수 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		// int num = sc.nextInt();
-		int num = 50;
-		int min = Math.min(num / 5, num / 2);
-		System.out.println(min);
+		int num = sc.nextInt();
+		int cnt = 0;
+		
+		while(num >= 5) {
+			num /= 5;
+			cnt+=num;
+			System.out.println("num:"+num);
+		}
+		
+		System.out.println(cnt);
 	}
 }
