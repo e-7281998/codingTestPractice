3ad034be5df43f6f6524b18ab3faa7d9277053d0
e-7281998 <jejjcs43@naver.com>
2023-04-22 23:51:15 +0900

    모의고사...

---
+++ src/codingTestPractice/Lv1/모의고사.java
@@ -0,0 +1,17 @@
+package codingTestPractice.Lv1;
+
+public class 모의고사 {
+
+	public static int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		int[] arr1 = {1,2,3,4,5};
+		int[] arr2 = {1,3,2,4,2};
+		
+		System.out.println(solution(arr1));	//1
+		System.out.println(solution(arr2));	//123
+	}
+}
